524a623608fab1563efb5574ab0e9eb836979400
Opilane TTHK <opilane@tthk.ee>
2025-10-31 10:48:05 +0200

    muuta

--- src/routes/overview.svelte
+++ src/routes/overview.svelte
@@ -1,3 +1,10 @@
+<script context="module">
+    export async function preload({params}, {token}) {
+        if (!token) {
+            this.redirect(302, '/login');
+        }
+    }
+</script>
 <script>
     import { post } from 'utils.js';
 
